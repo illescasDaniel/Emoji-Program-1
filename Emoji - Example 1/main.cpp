167d42faa3a2875bb2837884c8b50db7e1260977
Daniel ILLESCAS ROMERO <Daniel@daniel.home>
2016-06-13 15:23:27 +0200

    Initial Commit

---
+++ Emoji - Example 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Emoji - Example 1
+//
+//  Created by Daniel Illescas Romero on 13/06/16.
+//  Copyright © 2016 Daniel Illescas Romero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
